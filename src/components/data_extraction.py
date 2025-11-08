a2639e353f432e20fb574ed063b7a56995fceeb7
Kwamenas <knas68@gmail.com>
2025-11-08 17:05:04 +0100

    data_extraction

---
+++ src/components/data_extraction.py
@@ -0,0 +1,88 @@
+from src.logger_config.logger import logging
+from src.exception_config.exception import CustomException
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+import certifi ## provides a set of rules certification
+import json
+import os
+import sys
+from urllib.parse import quote_plus
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+
+class DataExtraction():
+    def __init__(self):
+        try:
+              load_dotenv()
+
+              user=os.getenv("MONGO_ATLAS_USER")
+              password=os.getenv("MONGO_ATLAS_PASSWORD")
+              host=os.getenv("MONGO_ATLAS_HOST")
+              params=os.getenv("MONGO_ATLAS_PARAMS","?retryWrites=true&w=majority")
+
+              self.database=os.getenv("DATABASE")
+              self.collection=os.getenv("COLLECTION")
+
+              if not all([user,password,host,self.database,self.collection]):
+                  raise ValueError ("Missing MongoDB credentials or database info in .env")
+              
+              password_quoted=quote_plus(password)
+              uri = f"mongodb+srv://{user}:{password_quoted}@{host}/{params}"
+
+              ca=certifi.where()
+
+              
+              self.client=MongoClient(uri,server_api=ServerApi('1'),tlsCAFile=ca)
+              logging.info("MongoDB client initialized successfully ")
+
+        except Exception as e:
+            raise CustomException(e)
+        
+    def csv_to_json_conventor(self,filepath:str):
+        try:
+            data=pd.read_csv(filepath)
+            data=data.reset_index(drop=True)
+            records=json.loads(data.to_json(orient='records'))
+            logging.info(f"Successfully converted {len(records)} rows from {filepath} to JSON")
+            return records
+        except Exception as e:
+            raise CustomException(e)
+        
+    def insert_into_MongoDB(self,records:list):
+        try:
+            db=self.client[self.database]
+            coll=db[self.collection]
+            results=coll.insert_many(records)
+            logging.info(f"Inserted {len(results.inserted_ids)} records into {self.database}.{self.collection}")
+            return (len(results.inserted_ids))  
+        except Exception as e:
+            raise CustomException(e)
+        
+    def extract_from_MongoDB(self):
+
+        try:
+            db=self.client[self.database]
+            coll=db[self.collection]
+            data=list(coll.find())
+            df=pd.DataFrame(data)
+            logging.info(f"fetched {len(df)} documents from {self.database}.{self.collection}")
+            return df
+        except Exception as e:
+            raise CustomException(e)
+        
+
+#if __name__=="__main__":
+ #   FILE_PATH="notebook\data\phisingData.csv"
+ #   DATABASE="KNAS68"
+ #   COLLECTION="NetworkData"
+ #   data_obj=DataExtraction()
+ #   record=data_obj.csv_to_json_conventor(filepath=FILE_PATH)
+  #  print(record)
+   # no_records=data_obj.insert_into_MongoDB(records=record,database=DATABASE,collection=COLLECTION)
+   # print(no_records)
+
+
---
+++ src/components/data_ingestion.py
@@ -0,0 +1,75 @@
+from src.logger_config.logger import logging
+from src.exception_config.exception import CustomException
+from src.components.data_extraction import DataExtraction
+import os
+import sys
+from dotenv import load_dotenv
+from dataclasses import dataclass
+from pathlib import Path
+import numpy as np
+import pandas as pd
+from pandas import DataFrame,Series
+from sklearn.model_selection import train_test_split
+
+load_dotenv()
+
+@dataclass
+
+class DataIngestionConfig:
+    
+    artifact_dir :Path= Path("artifacts")
+    feature_store:Path=artifact_dir/"feature_Store"
+    train_path: Path=artifact_dir/"train.csv"
+    valid_path: Path=artifact_dir/"valid.csv"
+    test_path: Path=artifact_dir/"test.csv"
+
+class DataIngest:
+    def __init__(self):
+        try:
+            self.data_ingest=DataIngestionConfig()
+            self.extractor=DataExtraction()
+
+            os.makedirs(self.data_ingest.feature_store,exist_ok=True)
+            os.makedirs(self.data_ingest.artifact_dir,exist_ok=True)
+
+            logging.info("DataIngest class initialized successfully.")
+        except Exception as e:
+            raise CustomException(e)
+
+    
+    def fetch_df_from_Mongo(self):
+        try:
+            df=self.extractor.extract_from_MongoDB()
+            logging.info(f"Successfully fetched {len(df)} records from MongoDB.")
+            df.to_csv(self.data_ingest.feature_store/"raw_data.csv",index=False)
+            logging.info(f"Data successfully fetched and saved to feature store")
+        except Exception as e:
+            raise CustomException(e)
+        
+    def split_data(self,df:DataFrame):
+        try:
+            logging.info("Train test split initiation")
+            train_set,temp_set=train_test_split(df,test_size=0.2,random_state=42)
+            valid_set,test_set=train_test_split(temp_set,test_size=0.5,random_state=42)
+
+            train_set.to_csv(self.data_ingest.train_path,index=False,header=True)
+            valid_set.to_csv(self.data_ingest.valid_path,index=False,header=True)
+            test_set.to_csv(self.data_ingest.test_path,index=False,header=True)
+
+            logging.info(f"Train data saved to {self.data_ingest.train_path}")
+            logging.info(f"Valid data saved to {self.data_ingest.valid_path}")
+            logging.info(f"Test data saved to {self.data_ingest.test_path}")
+            logging.info(f"Data Split Conpleted")
+
+            return train_set,valid_set,test_set
+        
+        except Exception as e:
+            raise CustomException(e)
+
+
+        
+        
+    
+
+
+
